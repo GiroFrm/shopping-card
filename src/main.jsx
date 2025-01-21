cc4b60d7c76e65471e20df8536e300501742d4d6
GiroFrm <girofram844@gmail.com>
2025-01-21 17:09:49 +1100

    git commit -m "feat: Add routing for App, Home, Products, and About components"

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NavBar from './components/NavBar.jsx';
+import Home from './pages/Home.jsx';
+import Products from './pages/Products.jsx';
+import About from './pages/About.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {path: "homepage", element: <Home/>},
+      {path: "products", element: <Products/>},
+      {path: "about", element: <About/>},
+    ]
+  },
+]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <RouterProvider router={router} />
   </StrictMode>,
 )
