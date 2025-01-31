8dd01fe412be75e24a8c82ac1c8662701d8f3c3a
GiroFrm <girofram844@gmail.com>
2025-01-31 14:15:28 +1100

    feat: src/hooks/useCategory.jsx

---
+++ src/hooks/useCategory.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+function useCategory(initialCategory) {
+  const [category, setCategory] = useState(initialCategory);
+  return { category, setCategory };
+}
+
+export default useCategory;
