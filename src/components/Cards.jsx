1dd68cb92de96518d1c5aef4df056e560db86647
GiroFrm <girofram844@gmail.com>
2025-02-07 16:55:41 +1100

    Add Cards component to display products based on category
    - Use `useData` hook to fetch data, handle loading and error states
    - Display loading and error messages
    - Render list of `Card` components with `flex` styling
    - Pass necessary props (`addIdProduct`, `addProductsCart`) to each `Card` component

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import Card from './Card'
+import {useData} from '../hooks/useData';
 
-function Cards() {
+function Cards({category, addIdProduct, addProductsCart}) {
+
+   const {data, error, loading} = useData(category);
+   
+   if (loading) {
+    return <h1>Loading Data...</h1>;
+  }
+
+  if (error) {
+    return <h1>Error: {error.message}</h1>;
+  }
+  
   return (
-    <div>cards</div>
+     
+    <div style={{display:'flex', flexWrap:'wrap', gap:'0.5rem'}}>
+     
+       {data && data.map(el=> (
+        <Card key={el.id} 
+              prop={el}
+              addIdProduct={addIdProduct}
+              addProductsCart={addProductsCart}
+              />  
+     ))}
+    </div>  
   )
 }
 
