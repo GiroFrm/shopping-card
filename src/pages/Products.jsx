651bdeca2b9075b208714eaf14c0c1f78a82236a
GiroFrm <girofram844@gmail.com>
2025-02-09 20:16:59 +1100

    feat: Add product categories and card components for product listing
    - Import necessary constants and styles
    - Utilize `useOutletContext` for context management
    - Implement category selection and display cards based on selected category

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+
+import { ELECTRONICS, JEWELRY, MENS_CLOTHING, WOMENS_CLOTHING } from '../constants/apiConstants';
+import '../style/Card.css'
+import '../style/Products.css'
+
+import { useOutletContext } from 'react-router-dom';
+
+import Cards from '../components/Cards.jsx';
+
 
 function Products() {
+  const { addIdProduct, addProductsCart, category, setCategory } = useOutletContext();
+
   return (
-    <div>Products page</div>
+    <div style={{ margin:'auto'}}>
+      <ul className='navBar products__categories' style={{cursor:"pointer", display:'flex', gap:"2rem", paddingBlock:"1rem", fontSize:"1.2rem"}}>
+        <li onClick={()=>setCategory(MENS_CLOTHING)}>Men's clothing</li>
+        <li onClick={()=>setCategory(WOMENS_CLOTHING)}>women's clothing</li>
+        <li onClick={()=>setCategory(ELECTRONICS)}>electronics</li>
+        <li onClick={()=>setCategory(JEWELRY)}>jewelery</li>
+      </ul>
+    
+      <Cards category= {category} 
+             addIdProduct={addIdProduct} 
+             addProductsCart={addProductsCart}/>  
+  </div>
   )
 }
 
