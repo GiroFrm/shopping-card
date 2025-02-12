e3530112997da2c05b16c62eaa7e1ba6e8ba8f9f
GiroFrm <girofram844@gmail.com>
2025-02-12 12:29:18 +1100

    feat: Add CheckOut component with cart functionality
    - Implemented the CheckOut component to display unique products in the cart.
    - Added functionality to increase and decrease product quantity.
    - Included feature to remove products from the cart.
    - Integrated utility function to calculate and approximate total price.
    - Styled the CheckOut component with corresponding CSS.

--- src/pages/CheckOut.jsx
+++ src/pages/CheckOut.jsx
@@ -1,11 +1,70 @@
 import React from 'react'
+import { FaTrashAlt } from 'react-icons/fa';
+import useCheckOut  from '../hooks/useCheckOut.jsx';
+import approxTotal from '../utils/util.js';
+import '../style/CheckOut.css';
 
 function CheckOut() {
+   
+  const {
+    uniqueProducts,
+    handleIncreaseQty,
+    handleDecreaseQty,
+    removeProductCart,
+    totalPriceApprox,
+    qty,
+  } = useCheckOut();
+
+  
+  function calculateTotal(price, qty) {
+    let total = Number(price) * Number(qty);
+    let rounded = approxTotal(total); 
+       return rounded;
+  }
+
+
   return (
     <div>
-        CheckOut Products
-        
+
+     {uniqueProducts && uniqueProducts.length > 0 ? 
+      <div className='checkOut'>
+
+      <div>
+       {uniqueProducts&& uniqueProducts.map(el => (
+       <div className="checkOut__list" key={el.id}>
+        <img src={el.image} alt={el.image} style={{width: '60px'}} />
+        <div>
+  
+        <p>{el.title} <span className='checkOut__price '>${calculateTotal(el.price, qty[el.id])}</span></p>
+         <div className='btnsContainer' style={{display: 'flex', alignItems:'center', gap:'0.3rem'}}>
+         <button onClick={()=>handleDecreaseQty(el.id)}>-</button>
+         <span>{qty[el.id]}</span>
+         <button onClick={()=>handleIncreaseQty(el.id)}>+</button>
+         <FaTrashAlt onClick={()=>removeProductCart(el.id)}/> 
+         </div>
         </div>
+       
+       </div>
+     ))}
+      </div>
+
+     <div class="checkOut__pay">
+        <div className='line-bottom'>
+        <h4>Order Summary</h4>
+        </div>
+        <div className='line-bottom'>
+        <p className='checkOut__total'>
+          <span>Total</span> <span className='checkOut__price'>${approxTotal(totalPriceApprox)}</span>
+        </p>
+        </div>
+        <div className='checkOut__button--container'>
+        <button className='checkout__button'>CheckOut</button>
+        </div>
+     </div>
+
+     </div> : <h1>Cart Empty</h1> }
+
+    </div>
   )
 }
 
