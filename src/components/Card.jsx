1e63bcc31f783d088e95b0c084e654d826cce0bf
GiroFrm <girofram844@gmail.com>
2025-02-14 08:33:03 +1100

    Add product card component and implement cart functionality
    - Created `Card` component to display product information.
    - Added `AddToCartButton` component to handle adding products to the cart.
    - Implemented state management for cart products and product IDs in the `App` component.
    - Created functions to add, remove, and manage products in the cart.
    - Integrated `NavBar` with cart count display.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import AddToCartButton from './AddToCarButton';
+import AddToCartButton from './AddToCartButton'
 
 function Card({prop, addIdProduct, addProductsCart}) {
 
   return (
     <div className='card'  style={{ padding: '0.3rem'}}>
     <div style={{ height:'280px', display:'grid', placeContent:'center'}}>
-    <img style={{maxWidth:'220px', maxHeight:'270px'}} src={prop.image} alt="" />
+    <img style={{maxWidth:'220px', maxHeight:'270px'}} src={prop.image} alt={prop.image} />
     </div>
     <p className='card__title'>{prop.title}</p>
     <p className='card__price'>${prop.price}</p>
