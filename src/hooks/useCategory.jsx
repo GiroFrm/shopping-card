62ca9158d95f8b9b2deac5227414e94a2d5addc3
GiroFrm <girofram844@gmail.com>
2025-02-16 18:02:38 +1100

    chore: Format code in useCategory hook
    - Adjusted indentation for consistency and readability.

--- src/hooks/useCategory.jsx
+++ src/hooks/useCategory.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 
 function useCategory(initialCategory) {
-  const [category, setCategory] = useState(initialCategory);
-  return { category, setCategory };
+    const [category, setCategory] = useState(initialCategory);
+    return { category, setCategory };
 }
 
 export default useCategory;
